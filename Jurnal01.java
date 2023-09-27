ea08a8ca412e4af1ccaa45c954539cea9c8c78ba
fqathf <100032449+fqathf@users.noreply.github.com>
2023-09-27 13:20:22 +0700

    Done

--- Jurnal01.java
+++ Jurnal01.java
@@ -1,6 +1,10 @@
 public class Jurnal01 {
     public static void main(String[] args) {
-        System.out.print("Hello" + " ");
-        System.out.println("World!");
+        String myName = "Faiq Athif Nurrahim Hadiko";
+        String myNIM = "607062330082";
+        String myClass = "D3IF-47-01";
+        System.out.println("Name: " + myName);
+        System.out.println("NIM: " + myNIM);
+        System.out.println("Class: " + myClass);
     }
 }
